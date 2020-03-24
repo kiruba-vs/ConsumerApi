f42211d0acb04a58d6666405dfdc5867c1e8efa6
kiruba-vs <59718658+kiruba-vs@users.noreply.github.com>
2020-03-23 21:20:42 -0500

    Updated Consumer.java

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,2 +1,56 @@
 public class Consumer {
+
+    private String Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phoneNumber;
+
+    public Consumer(String id, String firstName, String lastName, String email, int phoneNumber) {
+        Id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
